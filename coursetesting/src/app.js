2e84b8873e1bd8ba83251689a23f3043ad580d64
Peter Fischer <spotted@gmail.com>
2019-09-29 20:03:56 -0400

    adding effects

--- coursetesting/src/app.js
+++ coursetesting/src/app.js
@@ -1,14 +1,40 @@
 
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Note from './components/note'
+import axios from 'axios'
 
 //App module
-const App = ({notes : passedNotes}) => {
+const App = () => {
 
-  const [notes, setNotes] = useState(passedNotes);
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
+  //Use an effect to retrieve json data
+  // useEffect(() => {
+  //   console.log("Effect");
+  //   axios
+  //     .get("http://localhost:3001/notes")
+  //     .then(response => {
+  //       const {data : notes} = response;
+  //       setNotes(notes);
+  //     });
+  // }, []);
+
+   //Use an effect to retrieve json data
+  useEffect(() => {
+    console.log("Effect");
+    const fetchData = async () => {
+      const response = await axios.get("http://localhost:3001/notes");
+      setNotes(response.data);
+    }
+    fetchData();
+    }, []);
+  
+
+  console.log(`rendered `, notes.length, ` notes`);
+
+  // Show either all notes, or filtered notes
   const notesToShow = showAll ? notes : 
     notes.filter( (note) => note.important === true);
 
